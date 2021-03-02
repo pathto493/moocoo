5ebb9d3d7f7510e04b2d48520d8c3fece2bbe1e2
Nathaniel Santosa <nathaniel.santosa@gmail.com>
2021-03-02 16:43:13 +0800

    Create validations for orders model

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -1,4 +1,7 @@
 class Annotation < ApplicationRecord
   belongs_to :product
   belongs_to :video
+
+  validates :video, uniqueness: { scope: :product,
+    message: "One video can only have unique product" }
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,14 @@ class Order < ApplicationRecord
   belongs_to :user
 
   validates :quantity, presence: true, numericality: { only_integer: true }
+
+  validate :user_and_product_cannot_be_the_same
+
+  def user_and_product_cannot_be_the_same
+    other_orders = self.user.orders
+    bad_cart = other_orders.any? { |order| order.user_id == self.user_id && order.product_id == self.product_id }
+    if self.confirmed == false && bad_cart
+      errors.add(:bad_cart, 'you cannot have repeated instances of the same product in your cart, please modify the amount instead')
+    end
+  end
 end
