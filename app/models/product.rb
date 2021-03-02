e25b752bba864c4fb90a04f92b2e220f75a86d6d
Nathaniel Santosa <nathaniel.santosa@gmail.com>
2021-03-02 15:00:01 +0800

    Give validations for videos and products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   has_many :orders
   has_many :annotations
+
+  validates :name, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
 end
