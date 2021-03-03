e427b247c0f30a66c5100a2b8fccd82ed0dddadd
Regina Soh <syzregina@gmail.com>
2021-03-03 15:49:45 +0800

    adding code for index and home video page

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,22 @@
 class VideosController < ApplicationController
-  def index
+  def home
     @videos = Video.all
-
+    @videos_one = @videos[0]
+    @videos_two = @videos[1]
+    @videos_three = @videos[2]
+    @videos_four = @videos[3]
+    @videos_five = @videos[4]
+    @videos_six = @videos[5]
+    @videos_seven = @videos[6]
+    @videos_eight = @videos[7]
+    @videos_nine = @videos[8]
   end
 
+  def index
+    @videos = Video.all
+  end
+
+  def show
+    @videos = Video.all
+  end
 end
