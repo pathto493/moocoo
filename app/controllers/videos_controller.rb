a63d392f697de103f01ba352db52f1f7f0482df4
Regina Soh <syzregina@gmail.com>
2021-03-03 16:58:22 +0800

    Add creator to video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,15 +1,6 @@
 class VideosController < ApplicationController
   def home
     @videos = Video.all
-    @videos_one = @videos[0]
-    @videos_two = @videos[1]
-    @videos_three = @videos[2]
-    @videos_four = @videos[3]
-    @videos_five = @videos[4]
-    @videos_six = @videos[5]
-    @videos_seven = @videos[6]
-    @videos_eight = @videos[7]
-    @videos_nine = @videos[8]
   end
 
   def index
@@ -17,6 +8,6 @@ class VideosController < ApplicationController
   end
 
   def show
-    @videos = Video.all
+    @video = Video.find(params[:id])
   end
 end
