724a87ab5702c94061b025bc6327a9bd09c3f17d
Nathaniel Santosa <nathaniel.santosa@gmail.com>
2021-03-04 11:52:15 +0800

    Create checkout

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,8 +4,23 @@ class OrdersController < ApplicationController
   end
 
   def create
+    product = Product.find(params[:product][:product_id])
+    quantity = params[:product][:quantity]
+    order = Order.new(user: current_user, confirmed: false, quantity: quantity)
+    order.product = product
+    if order.save
+      redirect_to cart_path
+      flash.alert = "Successfully added to cart"
+    else
+      redirect_to product_path(product)
+      flash.alert = "You have added this to your cart already"
+    end
   end
 
   def destroy
+    order = Order.find(params[:id])
+    order.destroy
+    redirect_to cart_path
+    flash.alert = "Remove items from cart"
   end
 end
