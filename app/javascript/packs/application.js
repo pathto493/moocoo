acdc2e85f428577633f729b552a59ed5d01ce62d
Nathaniel Santosa <nathaniel.santosa@gmail.com>
2021-03-05 14:09:51 +0800

    Remove carousel thingy

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -33,7 +33,6 @@ import { playAhead } from './buttontest';
 
 // import { initSelect2 } from '../components/init_select2';
 import { initTabImageOnClick } from '../components/_tab_images';
-import { carousel } from '../components/_carousel';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
