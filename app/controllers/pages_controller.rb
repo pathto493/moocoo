809de616106e02e75d7490eee775c8070379f54b
cheyennelu <cheyenne.lu@gmail.com>
2021-03-05 18:10:35 +0800

    Home page button links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
 
   def home
     @user = current_user
+    @trends = %w[Natural Drama Eye]
+    @featured_products = %w[Lipstick Blusher Eyeliner]
   end
 
   def profile
