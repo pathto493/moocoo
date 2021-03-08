73c5436805be79824799ba2e105fe47fd3565258
timothy cheong <themomentisnow123@outlook.com>
2021-03-08 13:13:33 +0800

    need to make the product appear nicely in show page

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -2,7 +2,9 @@ class BrandsController < ApplicationController
   def index
     @brands = Brand.all
   end
+
   def show
     @brand = Brand.find(params[:id])
+    @products = Product.where(brand_id: params[:id])
   end
 end
