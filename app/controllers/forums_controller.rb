37f543c6a8fafac1e5588d419e4103748a55e16e
Regina Soh <syzregina@gmail.com>
2021-03-09 11:51:14 +0800

    add new topic button created

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -1,12 +1,32 @@
 class ForumsController < ApplicationController
    skip_before_action :authenticate_user!, only: [ :index, :show]
   def index
-    @topics = Forum.all
+    @topics = Forum.all.reverse
   end
 
   def show
     @topic = Forum.find(params[:id])
     @messages = Message.where(forum: @topic).reverse
     @message = Message.new
+  end
+
+  def new
+    @forum = Forum.new
+    @message = Message.new
+  end
+
+  def create
+    topic = params["Topic"]
+    topic_new = Forum.new(name: topic.capitalize)
+    if topic_new.save
+      title = params["Title"]
+      body = params["Body"]
+      if title.present? && body.present?
+        Message.create(title: title, content: body, user:current_user, date: Time.now, forum: topic_new)
+      end
+      redirect_to forums_path, notice: "Thread successfully created"
+    else
+      redirect_to new_forum_path, notice: "Missing Topic Title"
+    end
   end
 end
